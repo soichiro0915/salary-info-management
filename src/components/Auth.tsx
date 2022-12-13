a7ae5f79464a75223a4f7db15fcb08abc9ae12e2
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2022-12-13 14:22:38 +0900

    レイアウト修正

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,14 +1,17 @@
 import { signIn } from "next-auth/react";
+import { Button, Center } from "@mantine/core";
 
 export const Auth = () => {
   return (
     <div>
-      <button
-        className="rounded bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800"
-        onClick={() => signIn()}
-      >
-        Google Auth
-      </button>
+      <Center>
+        <Button
+          className="rounded bg-blue-600 py-2 px-4 font-bold text-white hover:bg-blue-800"
+          onClick={() => signIn()}
+        >
+          Google Auth
+        </Button>
+      </Center>
     </div>
   );
 };
